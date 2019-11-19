fd1bcc673da2e29eceb8d9bd4f81edf0b5988614
Naiji Zhang <naijizhang@gmail.com>
2019-11-18 21:22:36 -0800

    Create update-exclusion page

---
+++ src/pages/UpdateExclusion/UpdateExclusion.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function UpdateExclusion() {
+    return (
+        <div>
+            UpdateExclusion
+        </div>
+    )
+}
---
+++ src/pages/UpdateExclusion/index.js
@@ -0,0 +1,3 @@
+import UpdateExclusion from './UpdateExclusion';
+
+export default UpdateExclusion;
